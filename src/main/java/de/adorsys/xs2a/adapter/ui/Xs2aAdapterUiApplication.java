545de0c1c493e2f750ebc814e715b3ebbe129693
Mykola Shumyn <msh@adorsys.com.ua>
2019-12-24 14:20:54 +0200

    Created empty Spring Boot project

---
+++ src/main/java/de/adorsys/xs2a/adapter/ui/Xs2aAdapterUiApplication.java
@@ -0,0 +1,13 @@
+package de.adorsys.xs2a.adapter.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xs2aAdapterUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xs2aAdapterUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/adorsys/xs2a/adapter/ui/Xs2aAdapterUiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adorsys.xs2a.adapter.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xs2aAdapterUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
